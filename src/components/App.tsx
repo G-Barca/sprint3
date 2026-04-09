4b3990dc44e31f1ffa737904fbb96df08e2b507d
g-barca <oguilhermebarca@gmail.com>
2026-04-09 14:27:55 -0300

    Feat: novas páginas criadas

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Header } from './Header';
+
+import { Header } from './Header'; 
+import { Footer } from './Footer'; 
 import { Home } from '../pages/Home';
-import { Footer } from '../components/Footer'; 
 import { Integrantes } from '../pages/Integrantes';
+import { Sobre } from '../pages/Sobre';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-        <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/integrantes" element={<Integrantes />} />
-</Routes>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/integrantes" element={<Integrantes />} />
+        <Route path="/sobre" element={<Sobre />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
---
+++ src/pages/Sobre.tsx
@@ -0,0 +1,20 @@
+export function Sobre() {
+  return (
+    <main className="conteudo">
+      <section className="sobre">
+        <article>
+          <img src="/img/tdb.png" alt="Turma do Bem" />
+          <h1><strong>Ideia do projeto:</strong></h1>
+          <p>
+            A ideia do projeto consiste em desenvolver um website para pacientes que desejam agendar e consultar horários disponíveis pelo próprio site. Voluntários atualizam os horários que podem atender.
+          </p>
+          <p>
+            A ONG visualiza tudo num calendário central, onde pode manter a organização da agenda dos profissionais. 
+            As agendas dos dentistas ficam disponíveis no site, e o paciente pode escolher o dia e horário de sua preferência e fazer o agendamento sem depender de um atendente. <br /> 
+            Dessa forma, buscamos representar o início de uma solução tecnológica que poderia futuramente ser integrada a um sistema maior de gestão para a ONG Turma do Bem. O site foi desenvolvido utilizando conceitos básicos de front end, como a manipulação dos elementos HTML, estilização CSS e formulários com JavaScript; com menu de navegação, imagens, animações, textos, labels e formulários de registro.
+          </p>
+        </article>
+      </section>
+    </main>
+  );
+}
